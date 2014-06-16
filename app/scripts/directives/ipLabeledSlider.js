8173c8ea8bf5d68da5e4513615afc025ac8a1b6c
joe beuckman <joebeuckman@gmail.com>
2014-06-15 20:05:15 -0500

    slider directive coming along

--- app/scripts/directives/ipLabeledSlider.js
+++ app/scripts/directives/ipLabeledSlider.js
@@ -2,8 +2,21 @@
 angular.module('imagepoints').directive('ipLabeledSlider', function(){
     return {
         restrict: 'E',
+        scope: {
+            min: "@",
+            max: "@",
+            step: "@",
+            value: "@",
+            label: "@"
+        },
         link: function(scope, element, attributes) {
             console.log(attributes);
+            scope.min = attributes.min;
+            scope.max = attributes.max;
+            scope.step = attributes.step;
+            scope.value = attributes.value;
+            scope.label = element[0].innerText;
+            console.log(element[0]);
         },
         templateUrl: 'app/scripts/directives/ipLabeledSlider.html'
     }
