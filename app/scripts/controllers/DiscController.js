f071cf6e9580aec99139deecdbebf3cc9d6b8455
joe beuckman <joebeuckman@gmail.com>
2014-06-15 23:15:27 -0500

    operating on global vars from disc controller

--- app/scripts/controllers/DiscController.js
+++ app/scripts/controllers/DiscController.js
@@ -7,8 +7,46 @@ angular.module("imagepoints").controller("DiscController", ['$scope', function($
     $scope.brightnessEffect = 1.5;
 
     $scope.generateDiscPoints = function() {
-        alert('hi');
+
+        main_canvas.width = main_canvas.width;
+
+        discpoints_worker.postMessage({
+            cmd: 'begin',
+            image_data: image_data,
+            searchRadiusInner: $scope.minSpacing,
+            searchRadiusOuter: $scope.maxSpacing,
+            brightnessToDepth: $scope.brightnessEffect
+        });
     };
+
+
+    $scope.handleWorkerMessage = function(e) {
+
+        var data = e.data;
+
+        switch (data.cmd) {
+
+            case 'point_settled':
+                drawPoint(data.point, "#fff", data.point.radius);
+                break;
+
+            case "complete":
+                break;
+        }
+    };
+
+    $scope.drawPoint = function(p, color, radius) {
+
+        main_ctx.fillStyle = color;
+        main_ctx.beginPath();
+        main_ctx.arc(p.x, p.y, radius || .75, 0, Math.PI*2, true);
+        main_ctx.closePath();
+        main_ctx.fill();
+
+    };
+
+    discpoints_worker = new Worker('scripts/discpoints_worker.js?v=' + Math.random());
+    discpoints_worker.addEventListener('message', $scope.handleWorkerMessage, false);
 
 }]);
 
